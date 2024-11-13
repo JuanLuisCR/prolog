3ec23869417116fbf88ecd0993f92056db41cba2
juanluicrcr <juanluicrcr@gmail.com>
2024-11-13 09:50:37 -0600

    Otra manera de resolver ejercicio 1 de Prolog con reglas y hechos

--- dosEjerciciosProlog.pl
+++ dosEjerciciosProlog.pl
@@ -41,3 +41,24 @@ solucion(Hermano1, Mascota1, Hermano2, Mascota2, Hermano3, Mascota3, Hermano4, M
     Mascota1 \= Mascota2, Mascota1 \= Mascota3, Mascota1 \= Mascota4,
     Mascota2 \= Mascota3, Mascota2 \= Mascota4,
     Mascota3 \= Mascota4.
+
+
+    % Definimos los hermanos y las mascotas posibles hermano(ana). hermano(bruno). hermano(carla). hermano(daniel).
+
+mascota(perro). mascota(gato). mascota(loro). mascota(pez).
+
+% Regla que establece que cada hermano tiene una mascota única tienemascota(Hermano, Mascota) :- hermano(Hermano), mascota(Mascota).
+
+% Restricciones específicas según las pistas restricciones :- tienemascota(ana, MascotaAna), MascotaAna \= perro, MascotaAna \= pez,
+
+tienemascota(bruno, MascotaBruno), MascotaBruno \= gato,
+
+tienemascota(carla, MascotaCarla), MascotaCarla \= pez,
+
+tienemascota(daniel, MascotaDaniel), (MascotaDaniel = loro ; MascotaDaniel = pez).
+
+% Regla para asegurar que todos tienen mascotas distintas todosdiferentes(MascotaAna, MascotaBruno, MascotaCarla, MascotaDaniel) :- MascotaAna \= MascotaBruno, MascotaAna \= MascotaCarla, MascotaAna \= MascotaDaniel, MascotaBruno \= MascotaCarla, MascotaBruno \= MascotaDaniel, MascotaCarla \= MascotaDaniel.
+
+% Mostrar resultados mostrarresultados(MascotaAna, MascotaBruno, MascotaCarla, MascotaDaniel) :- format('Ana tiene un ~w~n', [MascotaAna]), format('Bruno tiene un ~w~n', [MascotaBruno]), format('Carla tiene un ~w~n', [MascotaCarla]), format('Daniel tiene un ~w~n', [MascotaDaniel]).
+
+% Regla principal que resuelve el problema resolver :- tienemascota(ana, MascotaAna), tienemascota(bruno, MascotaBruno), tienemascota(carla, MascotaCarla), tienemascota(daniel, MascotaDaniel), restricciones, todosdiferentes(MascotaAna, MascotaBruno, MascotaCarla, MascotaDaniel), mostrarresultados(MascotaAna, MascotaBruno, MascotaCarla, MascotaDaniel).
